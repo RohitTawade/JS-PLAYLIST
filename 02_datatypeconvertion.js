a14c86f4a61264afd60f359fc5f73e9de4432034
RohitTawade <rohittavade54@gmail.com>
2023-08-07 19:44:08 +0530

    js playlist

---
+++ 02_datatypeconvertion.js
@@ -0,0 +1,33 @@
+// let score = "33" // it will work
+// let score = "33abc"
+// let score = null // 0
+// let score = undefined // NaN
+// let score= true // 1
+// let score="Rohit" // NaN
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+
+//console.log(typeof(valueInNumber));
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0
+
+// let isLoggedIn = ""; // false
+let isLoggedIn = "Rohit"; // true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber); //33
+console.log(typeof(stringNumber)); // string
+
